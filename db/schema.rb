9545fb523d24a619e0cd2ffe5f1b87ae3f386c45
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-01 13:31:59 -0600

    add role migration, table, and necessary files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901180650) do
+ActiveRecord::Schema.define(version: 20180901192708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20180901180650) do
     t.string "skill_order"
     t.string "primary_class"
     t.string "secondary_class"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
