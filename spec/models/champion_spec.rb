3022da65f4ac4a08edace3233a78275563da12a4
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-01 16:35:20 -0600

    add model relationship tests for role and champion

--- spec/models/champion_spec.rb
+++ spec/models/champion_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Champion, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:name)}
   end
+  describe 'relationships' do
+    it {should have_many(:role_champions)}
+    it {should have_many(:roles)}
+  end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 RSpec.describe Role, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:title)}
+  end
+  describe 'relationships' do
+    it {should have_many(:role_champions)}
+    it {should have_many(:champions)}
   end 
 end
