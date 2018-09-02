dc676bb6f152a72cd8385e3ba901f43a1c0c22aa
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-02 15:42:40 -0600

    make roles index the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'roles#index'
   resources :roles, only: [:index] do
     resources :champions, only: [:index, :show]
   end
