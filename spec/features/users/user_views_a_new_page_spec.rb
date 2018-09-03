1f9b4fcbdb42c21ef2e6d35402267b61086263c6
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-02 22:16:15 -0600

    add new action method and successfully test presence of submit button

--- spec/features/users/user_views_a_new_page_spec.rb
+++ spec/features/users/user_views_a_new_page_spec.rb
@@ -6,13 +6,14 @@ describe 'User views a new user form' do
       visit '#root'
       click_on "Noob"
 
+      save_and_open_page
       expect(current_path).to eq(new_user_path)
       expect(page).to have_content("Please enter your info to start summoning smart.")
-      expect(page).to have_content("Name:")
-      expect(page).to have_content("Email:")
-      expect(page).to have_content("Password:")
-      expect(page).to have_content("Password Confirmation:")
-      expect(page).to have_content("Get started!")
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Password")
+      expect(page).to have_content("Password confirmation")
+      expect(page).to have_selector("input[type=submit][value='Get started!']")
     end
   end
 end
