dec5b52d726a42f070abaa56a2224fe675dc6eb5
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-02 19:53:41 -0600

    add role show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'roles#index'
-  resources :roles, only: [:index] do
+  resources :roles, only: [:index, :show] do
     resources :champions, only: [:index, :show]
   end
   resources :champions, only: [:index, :show]
