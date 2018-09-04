d82a9972c20f32d1f394f8a2f643792d88808aff
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-04 14:37:28 -0600

    add model test for user roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe User, type: :model do
     it {should have_many(:favorite_champions)}
     it {should have_many(:champions)}
   end
+
+  describe 'roles' do
+    it 'can be created as an admin' do
+      user = User.create(name: 'Hagrid', email: 'hagrid@hogwarts.com', password: 'dragon', role: 1)
+
+      expect(user.role).to eq('admin')
+      expect(user.admin?).to be_truthy
+    end
+    it 'can be created as a default user' do
+      user = User.create(name: 'Hermione', email: 'hermione@hogwards.com', password: 'leviosa', role: 0)
+
+      expect(user.role).to eq('default')
+      expect(user.default?).to be_truthy
+    end
+  end
 end
