afada97d18313b821dd8f32760cfdb6a173768ea
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-04 14:28:19 -0600

    add role to users to begin authorization methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904035552) do
+ActiveRecord::Schema.define(version: 20180904202556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180904035552) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
   end
 
   add_foreign_key "favorite_champions", "champions"
