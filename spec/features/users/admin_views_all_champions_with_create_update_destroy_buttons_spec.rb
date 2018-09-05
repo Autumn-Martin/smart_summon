8168f2c27199e889048d65990ccf2492afc00341
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-04 19:48:30 -0600

    build out admin view of champion index page spec

--- spec/features/users/admin_views_all_champions_with_create_update_destroy_buttons_spec.rb
+++ spec/features/users/admin_views_all_champions_with_create_update_destroy_buttons_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 feature 'User views all champions with create, update, and destroy buttons' do
   context 'as an admin' do
-      context 'user visits champions index page' do
+      context 'user visits admin champions index page' do
         scenario 'displays all champions' do
           champion_1 = create(:champion, name: "Poppy", damage: "tank")
           champion_2 = create(:champion, name: "Heimerdinger", damage: "magic")
+          admin = User.create(name: "Hagrid", email: "hagrid@hogwarts.com", password: "dragon", role: 1)
+
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
           visit admin_champions_path
 
           expect(page).to have_content(champion_1.name)
@@ -15,34 +19,42 @@ feature 'User views all champions with create, update, and destroy buttons' do
         end
       end
 
-    context 'when I view the champions index page' do
+    context 'when I view the admin champions index page' do
       scenario 'I also view create, update, and delete buttons for champions' do
-        admin = User.create(name: "Hagrid", email: "hagrid@hogwarts.com", password: "dragon")
+        admin = User.create(name: "Hagrid", email: "hagrid@hogwarts.com", password: "dragon", role: 1)
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
         visit admin_champions_path
 
-        expect(page).to have_content("Create")
-        expect(page).to have_content("Update")
-        expect(page).to have_content("Delete")
+        expect(page).to have_link("Create")
+        expect(page).to have_link("Update")
+        expect(page).to have_link("Delete")
       end
     end
   end
-  
+
   context 'as a default user' do
-    context 'when I view the champions index page' do
-      scenario 'I do not view create, update, and delete buttons for champions' do
-        admin = User.create(name: "Hermione", email: "hermione@hogwarts.com", password: "leviosa")
+    context 'when I view the admin champions index page' do
+      scenario 'I view a 404 not found page instead' do
+        champion_1 = create(:champion, name: "Poppy", damage: "tank")
+        champion_2 = create(:champion, name: "Heimerdinger", damage: "magic")
+        user = User.create(name: "Hermione", email: "hermione@hogwarts.com", password: "leviosa", role: 0)
 
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
         visit admin_champions_path
-
-        expect(page).to have_content("Create")
-        expect(page).to have_content("Update")
-        expect(page).to have_content(" Delete")
+        
+        expect(page).to have_content("The page you were looking for doesn't exist")
+        expect(page).to_not have_link("Create")
+        expect(page).to_not have_link("Update")
+        expect(page).to_not have_link("Delete")
+        expect(page).to_not have_content(champion_1.name)
+        expect(page).to_not have_content(champion_1.damage)
+        expect(page).to_not have_content(champion_2.name)
+        expect(page).to_not have_content(champion_2.damage)
       end
     end
   end
+
 end
