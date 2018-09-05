4af2452fa6408a93a3ad3520456c0ce00198bcaa
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-04 19:23:37 -0600

    add more feature tests for admin view of champions index page

--- spec/features/users/admin_views_all_champions_with_create_update_destroy_buttons_spec.rb
+++ spec/features/users/admin_views_all_champions_with_create_update_destroy_buttons_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 feature 'User views all champions with create, update, and destroy buttons' do
   context 'as an admin' do
+      context 'user visits champions index page' do
+        scenario 'displays all champions' do
+          champion_1 = create(:champion, name: "Poppy", damage: "tank")
+          champion_2 = create(:champion, name: "Heimerdinger", damage: "magic")
+          visit admin_champions_path
+
+          expect(page).to have_content(champion_1.name)
+          expect(page).to have_content(champion_1.damage)
+          expect(page).to have_content(champion_2.name)
+          expect(page).to have_content(champion_2.damage)
+        end
+      end
+
     context 'when I view the champions index page' do
       scenario 'I also view create, update, and delete buttons for champions' do
         admin = User.create(name: "Hagrid", email: "hagrid@hogwarts.com", password: "dragon")
@@ -16,6 +29,7 @@ feature 'User views all champions with create, update, and destroy buttons' do
       end
     end
   end
+  
   context 'as a default user' do
     context 'when I view the champions index page' do
       scenario 'I do not view create, update, and delete buttons for champions' do
